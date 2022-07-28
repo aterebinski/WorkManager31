d21b5a97a0a0a860bc24621cee2f39407cdc1e08
Adam Terebiński <aterebinski@wp.pl>
2022-07-28 12:05:15 +0200

    Setting a Limit to Change Username

--- WorkManager31/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ WorkManager31/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -30,6 +30,9 @@ namespace WorkManager31.Areas.Identity.Pages.Account.Manage
         [TempData]
         public string StatusMessage { get; set; }
 
+        [TempData]
+        public string UserNameChangeLimitMessage { get; set; }
+
         [BindProperty]
         public InputModel Input { get; set; }
 
@@ -74,6 +77,8 @@ namespace WorkManager31.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
+            UserNameChangeLimitMessage = $"You can change your username {user.UsernameChangeLimit} more time(s).";
+
             await LoadAsync(user);
             return Page();
         }
@@ -90,6 +95,30 @@ namespace WorkManager31.Areas.Identity.Pages.Account.Manage
             {
                 await LoadAsync(user);
                 return Page();
+            }
+
+            if (user.UsernameChangeLimit > 0)
+            {
+                if (Input.Username != user.UserName)
+                {
+                    var userNameExists = await _userManager.FindByNameAsync(Input.Username);
+                    if (userNameExists != null)
+                    {
+                        StatusMessage = "User name already taken. Select a different username.";
+                        return RedirectToPage();
+                    }
+                    var setUserName = await _userManager.SetUserNameAsync(user, Input.Username);
+                    if (!setUserName.Succeeded)
+                    {
+                        StatusMessage = "Unexpected error when trying to set user name.";
+                        return RedirectToPage();
+                    }
+                    else
+                    {
+                        user.UsernameChangeLimit -= 1;
+                        await _userManager.UpdateAsync(user);
+                    }
+                }
             }
 
             var firstName = user.FirstName;
