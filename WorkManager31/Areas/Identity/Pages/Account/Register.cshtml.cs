a64527e09637098b85e0798bc9894e4805931f8b
Adam Terebiński <aterebinski@wp.pl>
2022-08-01 12:13:42 +0200

    Add A Default Role to Newly Registered User

--- WorkManager31/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WorkManager31/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -96,6 +96,8 @@ namespace WorkManager31.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    await _userManager.AddToRoleAsync(user, Enums.Roles.Basic.ToString());
+
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
